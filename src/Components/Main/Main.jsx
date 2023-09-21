c9c8d125fb739d6a676ce7efbb04d5958c2ecf58
ikhtiyaratakishiyev <ikhtiyar.atakishiyev@gmail.com>
2023-09-21 23:23:18 +0400

    Finished Main section

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,10 +1,61 @@
 import React from 'react'
 
+import {travelData} from '../../Data/index'
+import {HiOutlineClipboardCheck, HiOutlineLocationMarker} from 'react-icons/hi'
 import "./main.scss"
+
+
 
 const Main = () => {
   return (
-    <div>Main</div>
+   <section className='main container section'>
+      <div className="secTitle">
+          <h3 className="title">
+              Most visited destination
+          </h3>
+      </div>
+
+      <div className="secContent grid">
+          {/* here I will use high order array method(map) */}
+         {
+          travelData.map(({id,imgSrc,destTitle,grade,location,fees,description})=>{
+             return(
+              <div key={id} className="singleDestination">
+                  <div className="imageDiv">
+                      <img src={imgSrc} alt={destTitle} />
+                  </div>
+
+                  <div className="cardInfo">
+                     <h4 className="destTitle">{destTitle}</h4>
+                     <span className="content flex">
+                        <span className="name">{location}</span>
+                        <HiOutlineLocationMarker className='icon'/>
+                     </span>
+
+                     <div className="fees flex">
+                        <div className="grade">
+                           <span>{grade}<small>+1</small></span>
+                        </div>
+                        <div className="price">
+                          <h5>{fees}</h5>
+                        </div>
+                     </div>
+
+                     <div className="desc">
+                        <p>{description}</p> 
+                     </div>
+
+                     <button className='btn flex'>
+                         DETAILS <HiOutlineClipboardCheck className='icon'/>
+                     </button>
+                  </div>
+              </div>
+             )
+          })
+         }
+      </div>
+   </section>
+
   )
 }
 
---
+++ src/Data/index.js
@@ -0,0 +1,93 @@
+import img1 from '../Assets/Images/img1.jpg'
+import img2 from '../Assets/Images/img2.jpg'
+import img3 from '../Assets/Images/img3.jpg'
+import img4 from '../Assets/Images/img4.jpg'
+import img5 from '../Assets/Images/img5.jpg'
+import img6 from '../Assets/Images/img6.jpg'
+import img7 from '../Assets/Images/img7.jpg'
+import img8 from '../Assets/Images/img8.jpg'
+import img9 from '../Assets/Images/img9.jpg'
+
+export const travelData = [
+    {
+        id:'1',
+        imgSrc:img1,
+        destTitle: 'Bora Bora',
+        location: 'New Zelland',
+        grade: ' Cultural Relax',
+        fees: '$700',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At eius accusamus sunt, facilis vitae delectus veniam nobis'
+    },
+    {
+        id:'2',
+        imgSrc:img2,
+        destTitle: 'Machu Picchu',
+        location: 'Peru',
+        grade: ' Cultural Relax',
+        fees: '$600',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At eius accusamus sunt, facilis vitae delectus veniam nobis'
+    },
+    {
+        id:'3',
+        imgSrc:img3,
+        destTitle: 'Great Barrier Reef',
+        location: 'Australia',
+        grade: ' Cultural Relax',
+        fees: '$700',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At eius accusamus sunt, facilis vitae delectus veniam nobis'
+    },
+    {
+        id:'4',
+        imgSrc:img4,
+        destTitle: 'Cappadokia',
+        location: 'Turkey',
+        grade: ' Cultural Relax',
+        fees: '$800',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At eius accusamus sunt, facilis vitae delectus veniam nobis'
+    },
+    {
+        id:'5',
+        imgSrc:img5,
+        destTitle: 'Guanajuato',
+        location: 'Mexico',
+        grade: ' Cultural Relax',
+        fees: '$1100',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At eius accusamus sunt, facilis vitae delectus veniam nobis'
+    },
+    {
+        id:'6',
+        imgSrc:img6,
+        destTitle: 'Cinque Terre',
+        location: 'Italy',
+        grade: ' Cultural Relax',
+        fees: '$840',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At eius accusamus sunt, facilis vitae delectus veniam nobis'
+    },
+    {
+        id:'7',
+        imgSrc:img7,
+        destTitle: 'Angkor Wat',
+        location: 'Cambodia',
+        grade: ' Cultural Relax',
+        fees: '$790',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At eius accusamus sunt, facilis vitae delectus veniam nobis'
+    },
+    {
+        id:'8',
+        imgSrc:img8,
+        destTitle: 'Taj Mahal',
+        location: 'India',
+        grade: ' Cultural Relax',
+        fees: '$900',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At eius accusamus sunt, facilis vitae delectus veniam nobis'
+    },
+    {
+        id:'9',
+        imgSrc:img9,
+        destTitle: 'Bali Island',
+        location: 'Indonesia',
+        grade: ' Cultural Relax',
+        fees: '$500',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At eius accusamus sunt, facilis vitae delectus veniam nobis'
+    },
+]
